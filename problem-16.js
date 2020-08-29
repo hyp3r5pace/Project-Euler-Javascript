fe00f16520f06d25767effc37189107dfec0e524
Soumyajit Deb <debsoumyajit100@gmail.com>
2020-08-29 21:37:39 +0530

    Add solution for problem 16

--- problem-16.js
+++ problem-16.js
@@ -1,6 +1,5 @@
 "use strict";
-let number = BigInt(2 ** 1000);
-let str = String(number), sum = 0;
+let number = BigInt(2 ** 1000), str = String(number), sum = 0;
 for (let i = 0; i < str.length; i++) sum += parseInt(str[i]);
 
 console.log(sum);
